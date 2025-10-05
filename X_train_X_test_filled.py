06a811b45b765f4971d7e70a7315e847778c06c7
Thomas MacPherson <tmacphe@umich.edu>
2025-10-05 10:06:23 -0600

    kept Ticker index in imputation file

--- X_train_X_test_filled.py
+++ X_train_X_test_filled.py
@@ -1,3 +1,9 @@
+'''
+Pipeline File 5
+
+Imputation based on Sector and Market cap Clustering
+
+'''
 import pandas as pd
 import argparse
 
@@ -12,37 +18,40 @@ if __name__ == "__main__":
     # Load datasets
     df_train = pd.read_csv(args.train_input)
     df_test = pd.read_csv(args.test_input)
-    print(df_train.head())
 
-    ## Fundamental prefixes
-    #fundamental_prefixes = [
-    #    "CapitalExpenditure", "CashAndSTInvestments", "CashFromOps", "CostOfRevenue",
-    #    "CurrentAssets", "CurrentLiabilities", "EPS", "IncomeTaxExpense", "InterestExpense",
-    #    "LongTermDebt", "NetIncome", "OperatingIncome", "OtherOperatingExpense",
-    #    "Revenue", "TotalAssets", "TotalDebt", "TotalEquity", "TotalLiabilities"
-    #]
-#
-    ## Pick relevant columns
-    #target_cols_old = [c for c in df_train.columns if any(c.startswith(p + "_") for p in fundamental_prefixes)]
-    #target_cols_new = [c for c in df_test.columns if any(c.startswith(p + "_") for p in fundamental_prefixes)]
-#
-    ## Fill missing values for training set
-    #df_original_train = df_train.copy()
-    #for col in target_cols_new:
-    #    df_original_train[col] = df_original_train.groupby(['Sector','Market Cap'])[col].transform(lambda s: s.fillna(s.median()))
-    #df_original_train = df_original_train.dropna()
-    #df_original_train.to_csv(args.train_output, index=False)
-#
-    ## Fill missing values for test set using medians from training set
-    #df_new_test = df_test.copy()
-    #for col in target_cols_new:
-    #    medians = df_train.groupby(['Sector','Market Cap'])[col].median()
-    #    df_new_test[col] = df_new_test.apply(
-    #        lambda row: medians.get((row['Sector'], row['Market Cap']), pd.NA) if pd.isna(row[col]) else row[col],
-    #        axis=1
-    #    )
-    #df_new_test = df_new_test.dropna()
-    #df_new_test.to_csv(args.test_output, index=False)
-#
-    #print(f"Original-style output saved to {args.train_output}")
-    #print(f"New-style output saved to {args.test_output}")
+
+    # Fundamental prefixes
+    fundamental_prefixes = [
+        "CapitalExpenditure", "CashAndSTInvestments", "CashFromOps", "CostOfRevenue",
+        "CurrentAssets", "CurrentLiabilities", "EPS", "IncomeTaxExpense", "InterestExpense",
+        "LongTermDebt", "NetIncome", "OperatingIncome", "OtherOperatingExpense",
+        "Revenue", "TotalAssets", "TotalDebt", "TotalEquity", "TotalLiabilities"
+    ]
+
+    # Pick relevant columns
+    target_cols_old = [c for c in df_train.columns if any(c.startswith(p + "_") for p in fundamental_prefixes)]
+    target_cols_new = [c for c in df_test.columns if any(c.startswith(p + "_") for p in fundamental_prefixes)]
+
+    # Fill missing values for training set
+    df_original_train = df_train.copy()
+    for col in target_cols_new:
+        df_original_train[col] = df_original_train.groupby(['Sector','Market Cap'])[col].transform(lambda s: s.fillna(s.median()))
+    df_original_train = df_original_train.dropna()
+    
+    df_original_train.set_index('Ticker',inplace=True)
+    df_original_train.to_csv(args.train_output, index=True)
+
+    # Fill missing values for test set using medians from training set
+    df_new_test = df_test.copy()
+    for col in target_cols_new:
+        medians = df_train.groupby(['Sector','Market Cap'])[col].median()
+        df_new_test[col] = df_new_test.apply(
+            lambda row: medians.get((row['Sector'], row['Market Cap']), pd.NA) if pd.isna(row[col]) else row[col],
+            axis=1
+        )
+    df_new_test = df_new_test.dropna()
+    df_new_test.set_index('Ticker',inplace=True)
+    df_new_test.to_csv(args.test_output, index=True)
+
+    print(f"Original-style output saved to {args.train_output}")
+    print(f"New-style output saved to {args.test_output}")
